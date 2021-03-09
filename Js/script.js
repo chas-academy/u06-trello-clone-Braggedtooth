1215633bdfd57dd88c9fbdb0bdf36e25b0f18082
Braggedtooth <54339202+Braggedtooth@users.noreply.github.com>
2021-03-09 17:45:41 +0100

    added function to add new card and new list

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,30 @@
 $(document).ready(() => {
-        $('.list').draggable();
+    $('.list-content').draggable({
+        connectToSortable: "#sortable",
+        snapMode:"inner",
+			revert: "invalid"
+       
+   }); 
+    
+    $('#sortable').sortable({
+        revert: true
+    })
+       
     });
+ 
+$('.input').keypress((event) => {
+    if (event.which ===13) {
+        let newTodo = $('.input').val();
+        $('#todo').append('<li class="card-content list-content card ui-draggable ui-draggable-handle ui-sortable">' + newTodo + '</li>')
+    }
+});
+
+$(".droppable").droppable({
+    accept: "#sortable li",
+    drop: function (event, ui) {
+            $(this).appendToParent( ''); 
+  
+        }
+});
+
+let title = $
