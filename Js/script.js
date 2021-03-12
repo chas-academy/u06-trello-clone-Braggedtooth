62017b91b9ef260fc593f1b8c325e9429e078032
Braggedtooth <54339202+Braggedtooth@users.noreply.github.com>
2021-03-12 02:44:15 +0100

    Added ability to create new lists added icons

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     $('.list-content').draggable({
-        connectToSortable: "#sortable",
-        snapMode:"inner",
+            connectToSortable: "#sortable",
+            snapMode:"inner",
 			revert: "invalid"
        
    }); 
@@ -10,21 +10,166 @@ $(document).ready(() => {
         revert: true
     })
        
-    });
- 
-$('.input').keypress((event) => {
-    if (event.which ===13) {
-        let newTodo = $('.input').val();
-        $('#todo').append('<li class="card-content list-content card ui-draggable ui-draggable-handle ui-sortable">' + newTodo + '</li>')
-    }
 });
+    
 
-$(".droppable").droppable({
-    accept: "#sortable li",
-    drop: function (event, ui) {
-            $(this).appendToParent( ''); 
-  
+
+
+
+    
+
+function contains(a, obj) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] === obj) {
+            return true;
         }
+    }
+    return false;
+}   
+let title;
+let list = [];
+let listId;
+
+const addNewList = () => {
+        $('.list-wrapper').append('<section class=" newList column list"> <span class="icon"> <i class="delete"></i> </span> <span class=" subtitle list-header">'+title+'</span><ul id="'+title+'" class="todo sortable">  </ul><input class="input '+ title+'" type="text" placeholder="Add New"></section> ')
+        list.push(title);
+    }
+
+   
+
+                   
+                       
+                   
+                   
+
+                    
+
+                  
+
+
+$('#create-list').on('click', () => {
+    title = $('#list-title').val();
+    let titleExist = contains(list,title);
+    if (titleExist === true) {
+         title = prompt('List already Exist,Choose a new name')
+           return addNewList();
+    } else   addNewList();
+          
+  
+   /*   for (let i = 0; i < list.length; i++) {
+         listId =  title+ list[i].length
+        
+    } */
+
+  
+});
+//DEFAULTS 
+
+$('#addTodo').keypress((event) => {
+    if (event.which === 13) {
+        let newTodo = $('#addTodo').val();
+   
+        const addNewTodo = () => {
+            $('#todo').append('<li class="card-content list-content card">' + newTodo + '</li>')
+        }
+          if (newTodo === "") {
+              newTodo = prompt('What do you want to add?')
+              return addNewTodo();
+            
+            
+            } else addNewTodo();
+                    
+       
+    }
+    
 });
 
-let title = $
+$('#addDoing').keypress((event) => {
+    if (event.which === 13) {
+        let newTodo = $('#addDoing').val();
+   
+        const addNewTodo = () => {
+            $('#doing').append('<li class="card-content list-content card">' + newTodo + '</li>');
+        }
+          if (newTodo === "") {
+              newTodo = prompt('What do you want to add?')
+              return addNewTodo();
+            
+            
+            } else addNewTodo();
+                    
+    }
+      
+    
+});
+$('#addDone').keypress((event) => {
+    if (event.which === 13) {
+        let newTodo = $('#addDone').val();
+   
+        const addNewTodo = () => {
+            $('#done').append('<li class="card-content list-content card">' + newTodo + '</li>');
+        }
+          if (newTodo === "") {
+              newTodo = prompt('What do you want to add?')
+              return addNewTodo();
+            
+            
+            } else addNewTodo();
+                    
+       
+    }
+    
+});
+
+
+    $('.delete').on('click', () => {
+        $('.delete').parent().remove()
+        
+        });
+    
+    
+
+
+//DEFAULTS
+
+/* const listItemSubmit = (id) => {
+    id.keypress((event) => {
+        if (event.which === 13) {
+            let newTodo = id.val();
+            const addNewTodo = () => {
+                id.append('<li class="card-content list-content card">' + newTodo + '</li>');
+            }
+            if (newTodo === "") {
+                newTodo = prompt('What do you want to add?')
+                return addNewTodo(id);
+            
+            
+            } else addNewTodo(id);
+                    
+       
+        }
+        
+    })
+};
+
+listItemSubmit(title);
+
+$('newList').add() */
+
+/*  $('.addTodo').keypress((event) => {
+    if (event.which === 13) {
+        let newTodo = $('.input').val();
+        const addNewTodo = () => {
+            id.append('<li class="card-content list-content card">' + newTodo + '</li>');
+        }
+          if (newTodo === "") {
+              newTodo = prompt('What do you want to add?')
+              return addNewTodo(title);
+            
+            
+            } else addNewTodo(title);
+                    
+       
+    }
+    
+});  */
