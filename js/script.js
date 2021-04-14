e21faa102dfae8034ee3e4814ea6340453ab4c87
Braggedtooth <54339202+Braggedtooth@users.noreply.github.com>
2021-04-14 21:30:01 +0200

    feat: Dialog

--- js/script.js
+++ js/script.js
@@ -21,24 +21,98 @@ let taskId = 0;
 
 let taskTitle;
 let taskDesc;
+const dialog = $('#dialog');
 
 function createTask(taskId, taskTitle, taskDesc) {
-    const task = $('<li>').addClass('task').attr('id', 'task-${taskId}');
-    const taskHeader = $('<h3>').addClass('card-header').val(taskTitle);
-    const taskBody = $('p').addClass('card-body').val(taskDesc);
+    const tId = `task-${taskId}`
+    const task = $('<li>').addClass('task').attr('id', tId);
+    const taskHeader = $('<h3>').addClass('card-header').text(taskTitle);
+    const taskBody = $('<p>').addClass('card-body').text(taskDesc);
     $('.todo').append(task);
-    $('task').append(taskHeader);
-    $('task').append(taskBody);
+    task.append(taskHeader);
+    task.append(taskBody);
+    tasks = $('.task');
+    taskEvent();
 
 }
 
-$('.navbuttons').on('click', '.newTask', (taskId) => {
 
-    console.log(taskId);
-    taskTitle = $('#title').val;
-    taskDesc = $('#desc').val;
+
+
+
+
+$('#tabs').tabs();
+
+function getId() {
+    tasks.each(function () {
+        return taskId++;
+    })
+}
+
+
+$('.navbuttons').on('click', '.newTask', function () {
+    getId();
+    taskTitle = $('#title').val();
+    taskDesc = $('#desc').val();
     createTask(taskId, taskTitle, taskDesc);
-})
+    console.log(taskTitle, taskDesc)
+});
+
+
+
+let clickedItemId;
+function taskEvent() {
+    tasks.on('click', function () {
+        title = $(this).find('h3').html();
+        desc = $(this).find('p').text();
+        console.table(title, '', desc);
+        dialog.dialog('open')
+        clickedItemId = $(this).attr('id')
+
+    });
+}
+
+
+
+dialog.dialog({
+    modal: true,
+    width: 500,
+    height: 350,
+    title: 'Edit Todo',
+
+    resizeble: false,
+    draggable: false,
+    autoOpen: false,
+    show: {
+        effect: "fade",
+        duration: 100
+    },
+    hide: {
+        effect: "fade",
+        duration: 100
+    },
+    open: function () {
+        $('#taskTitle').val(title);
+        $('#taskDesc').val(desc);
+    },
+    buttons: {
+        'Save': function () {
+            title = $('#taskTitle').val();
+            desc = $('#taskDesc').val();
+            $('.card-header').val(title);
+            setNewValues();
+        }
+    }
+});
+
+function setNewValues() {
+    const newValueId = '#' + clickedItemId;
+    changingTitle = $(newValueId).find('h3')
+    changingDesc = $(newValueId).find('p')
+
+    $(changingTitle).val(title);
+    $(changingDesc).val(desc);
+}
 
 
 
@@ -56,16 +130,16 @@ $('.navbuttons').on('click', '.newTask', (taskId) => {
 /*
 $("#dialog").dialog({
 
-    autoOpen: false,
+            autoOpen: false,
     draggable: false,
     modal: true,
     buttons: {
-        close: function () {
+            close: function () {
             $(this).dialog("close");
         }
     },
     hide: {
-        effect: "clip",
+            effect: "clip",
         duration: 350
     }
 });
@@ -76,15 +150,10 @@ $('#dialog').append(tabs);
 
 $('.task').on('click', function () {
 
-    el = $(this)
+            el = $(this)
     console.log(el);
     $('#dialog').dialog("open");
 
 });
  */
-
-
-
-
-
-
+taskEvent();
